f639cc0a23ca63d5e838ddebc0a8225401538e6b
yalandr <52127093+yalandr@users.noreply.github.com>
2020-08-20 23:18:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//----Burger Menu------------------------
+
+$(function(){
+	
+	$('.menu__icon').on('click', function(){
+		
+		$('.menu__burger').slideToggle(800, function(){
+			
+			if($(this).css('display') === 'none'){
+				$(this).removeAttr('style');
+		}
+		
+	});
+	
+ });
+	
+});
+
+//==== Closing menu =====================================
+
+ $(function(){
+ 	$('.close__icon').on('click', function(){
+ 		$('.menu__burger').hide(800);
+ 	});
+ });
+
+//------ Thanks for order---------------------------------
+
+document.querySelector('.submit__btn').onclick = function(event) {
+	event = event || window.event;
+	alert('Спасибо за обращение! Мы Вам скоро перезвоним.');
+};
